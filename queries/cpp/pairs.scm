1a4207e79c7ce77cc1fceafb8e68b2a4891322f1
Stephan Seitz <stephan.seitz@fau.de>
2021-04-21 11:09:03 +0200

    Let C++ inherit from C

--- queries/cpp/pairs.scm
+++ queries/cpp/pairs.scm
@@ -1,13 +1,1 @@
-; inherits: curly
-
-(preproc_if
-  "#if"    @left
-  "#endif" @right)
-
-(preproc_ifdef
-  "#ifdef" @left
-  "#endif" @right)
-
-(preproc_ifdef
-  "#ifndef" @left
-  "#endif" @right)
+; inherits: c
